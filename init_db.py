f994c1b28569001a176b61c1952d9441a0cfbf15
Dina Abd El-Halim <91435311+Dinahalem@users.noreply.github.com>
2023-05-20 22:17:44 +0300

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -1,9 +1,8 @@
 import psycopg2
 
-conn = psycopg2.connect(database="postgres", host="postgres-server", user="postgres", password="1234", port="5432")
+conn = psycopg2.connect(database="db_flask", host="postgres-server", user="postgres", password="1234", port="5432")
 cur = conn.cursor()
 
-cur.execute("CREATE DATABASE flask_db")
 cur.execute('''CREATE TABLE IF NOT EXISTS courses (id serial PRIMARY KEY, name varchar(100), fees integer, 
 duration integer);''')
 cur.execute('''INSERT INTO courses (name, fees,duration) VALUES ('python',6500,45),('Java', 7000,60),('Javascript',
